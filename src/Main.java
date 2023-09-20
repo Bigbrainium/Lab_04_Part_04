b2e9166c07927e0cfe47a4a33341dfc550d75aec
Bigbrainium <prashunregmi15@gmail.com>
2023-09-20 09:03:14 -0400

    Lab_04_Part_2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int cardBalance = 5000;
+        double interestRate = 0.17;
+
+        // Variables
+
+        double firstMonth = cardBalance * interestRate;
+        double firstMonthTotal = firstMonth + cardBalance;
+        double secondMonth = firstMonth * interestRate;
+        double secondMonthTotal = secondMonth + firstMonthTotal;
+
+        System.out.println("The first month total is: " + firstMonthTotal);
+        System.out.println("The second month total is: " + secondMonthTotal);
+
     }
 }
